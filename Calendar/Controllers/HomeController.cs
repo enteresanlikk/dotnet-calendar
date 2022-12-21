b761254c9cfe97326ef8a0eedb232d77ef4eee81
enteresanlikk <zeki1198@gmail.com>
2022-12-21 23:22:14 +0300

    init.

---
+++ Calendar/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Calendar.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int year = 0, int month = 0)
+        {
+            var now = DateTime.Now;
+            var _year = year != 0 ? year : now.Year;
+            var _month = month != 0 ? month : now.Month;
+
+            ViewBag.Year = _year;
+            ViewBag.Month = _month;
+
+            return View();
+        }
+    }
+}
